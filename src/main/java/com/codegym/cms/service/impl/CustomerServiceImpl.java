b509a7da3ac125e37c4c12aac962be61dfe3b113
minhdien1710 <nguyenminhdien996@gmail.com>
2019-10-02 11:31:14 +0700

    pagging

--- src/main/java/com/codegym/cms/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/codegym/cms/service/impl/CustomerServiceImpl.java
@@ -5,14 +5,16 @@ import com.codegym.cms.model.Province;
 import com.codegym.cms.repository.CustomerRepository;
 import com.codegym.cms.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
     @Override
-    public Iterable<Customer> findAll() {
-        return customerRepository.findAll();
+    public Page<Customer> findAll(Pageable pageable) {
+        return customerRepository.findAll(pageable);
     }
 
     @Override
@@ -28,6 +30,11 @@ public class CustomerServiceImpl implements CustomerService {
     @Override
     public void remove(Long id) {
         customerRepository.delete(id);
+    }
+
+    @Override
+    public Page<Customer> findAllByFirstNameContaining(String firstName, Pageable pageable) {
+        return customerRepository.findAllByFirstNameContaining(firstName,pageable);
     }
 
     @Override
